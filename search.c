df84de0f38fa171c230150e9e75aa30ecc0e1c79
ben <ben@bens-MacBook-Pro.local>
2020-02-21 21:06:14 +0100

    Search refactor

--- search.c
+++ search.c
@@ -26,7 +26,7 @@ uint16_t bestMove(struct Position *pos, struct Info info)
     unsigned int startTime = gettimems(), depth;
     uint16_t ttMove; int ttEval, ttDepth, ttFlag;
 
-    for (depth = 1; gettimems() - startTime < 1000; depth++)
+    for (depth = 1; gettimems() - startTime < 100; depth++)
     {
         search(pos, depth, 0, -MATE, MATE);
 
@@ -53,55 +53,63 @@ int search(struct Position *pos, int depth, int height, int alpha, int beta)
     int alphaOrig = alpha;
 
     uint16_t ttMove; int ttEval, ttDepth, ttFlag;
+
     if (getTT(pos->state->zobrist, &ttMove, &ttEval, &ttDepth, &ttFlag) && ttDepth >= depth)
     {
         ttEval = evalFromTT(ttEval, height);
 
-        if      (ttFlag == EXACT)                  return ttEval;
-        else if (ttFlag == LOWERBOUND) alpha = MAX(alpha, ttEval);
-        else if (ttFlag == UPPERBOUND) beta  = MIN(beta,  ttEval);
+        if      (ttFlag == EXACT)                         return ttEval;
+        else if (ttFlag == LOWERBOUND && ttEval > alpha) alpha = ttEval;
+        else if (ttFlag == UPPERBOUND && ttEval < beta)  beta  = ttEval;
 
-        if (alpha >= beta) return ttEval;
+        if (alpha >= beta) 
+        {
+            return ttEval;
+        }
     }
 
-    if (depth == 0) return evalPos(pos);
+    if (depth == 0)
+    {
+        return evalPos(pos);
+    }
 
     uint16_t moveList[MAX_MOVES];
     uint16_t *begin = moveList;
     uint16_t *end   = generateLegalMoves(pos, moveList);
+    
     if (begin == end)
     {
         // mate or stalemate
         return squareIsAttacked(__builtin_ctzll(pos->piece[KING] & pos->color[pos->state->turn]), !pos->state->turn, pos) ? -MATE + height : 0;
     }
 
-    struct State newState;
-    uint16_t bestMove; int eval, flag, nega;
-    for (bestMove = *begin, eval = -MATE; begin < end; begin++)
+    struct State newState; int eval;
+    for (ttMove = *begin, ttEval = -MATE; begin < end; begin++)
     {
         doMove(*begin, pos, &newState);
-
-        nega = -search(pos, depth - 1, height + 1, -beta, -alpha);
-
-        eval  = MAX(eval, nega);
-
-        if (eval > alpha) 
-        {
-            bestMove = *begin;
-            pvList[height] = bestMove;
-        }
-
-        alpha = MAX(eval, alpha);
-
+        eval = -search(pos, depth - 1, height + 1, -beta, -alpha);
         undoMove(*begin, pos);
 
-        if (alpha >= beta) break;
+        if (eval > ttEval)
+        {
+            ttEval = eval;
+            if (eval > alpha) 
+            {
+                alpha = eval;
+                ttMove = pvList[height] = *begin;
+            }
+        }
+
+        if (alpha >= beta)
+        {
+            break;
+        }
     }
 
-    if      (eval <= alphaOrig) flag = UPPERBOUND;
-    else if (eval >= beta)      flag = LOWERBOUND;
-    else                        flag = EXACT;
-    putTT(pos->state->zobrist, bestMove, evalToTT(eval, height), depth, flag);
+    ttFlag = ttEval <= alphaOrig ? UPPERBOUND
+           : ttEval >= beta      ? LOWERBOUND
+           : EXACT;
+    putTT(pos->state->zobrist, ttMove, evalToTT(ttEval, height), depth, ttFlag);
 
-    return eval;
+    return ttEval;
 }
