9d1ac494dbac3efab7a6bccd06a7b33db99477d6
ben <ben@bens-MacBook-Pro.local>
2020-02-21 22:07:11 +0100

    Added piece placement value tables

--- search.h
+++ search.h
@@ -9,7 +9,7 @@ int evalPos(struct Position *pos);
 uint16_t bestMove(struct Position *pos, struct Info info);
 int search(struct Position *pos, int depth, int height, int alpha, int beta);
 
-#define MAX(A, B) ((A) > (B) ? (A) : (B))
-#define MIN(A, B) ((A) < (B) ? (A) : (B))
+
+int PIECESQUARE[PIECE_N][SQUARE_N];
 
 #endif
